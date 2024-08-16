6e8c0ebc884e6618ec78ed6c53f2ac4cb73553ca
Minh Quy <onequy@gmail.com>
2024-08-16 07:23:44 +0700

    bg

--- src/App/Screens/Background/BackgroundNavigator.tsx
+++ src/App/Screens/Background/BackgroundNavigator.tsx
@@ -15,9 +15,8 @@ const BackgroundNavigator = () => {
         >
             <StatusBar translucent backgroundColor="transparent" />
 
-            <ImageBackground
-                style={CommonStyles.width100PercentHeight100Percent}
-                source={{ uri: 'https://images.unsplash.com/photo-1487147264018-f937fba0c817?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+            <View
+                style={[CommonStyles.width100PercentHeight100Percent, { backgroundColor: 'whitesmoke'}]}
             />
         </View>
     )
