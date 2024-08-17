30676daa9a7c536ba618e4a41ee38d91468f1280
Minh Quy <onequy@gmail.com>
2024-08-17 13:30:02 +0700

    flexingStartAnimation

--- src/App/Components/TaskView/TaskItemView.tsx
+++ src/App/Components/TaskView/TaskItemView.tsx
@@ -1,16 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useMemo } from 'react'
+import { View, Text, StyleSheet, Animated } from 'react-native'
+import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import { TaskPersistedAndRuntimeData, TaskPersistedData } from '../../Types'
+import useAnimatedValue from '../../../Common/Hooks/useAnimatedValue'
+import { SafeValue } from '../../../Common/UtilsTS'
 
 const TaskItemView = ({
     task
 }: {
     task: TaskPersistedAndRuntimeData
 }) => {
+    const onFlexingAnimationEnd = useCallback((currentValue: number) => {
+
+    }, [])
+
+    const {
+        animatedValue: flexingAnimatedValue,
+        startAnimation: flexingStartAnimation,
+        currentValueRef: flexingCurrentValueRef
+    } = useAnimatedValue(0, onFlexingAnimationEnd)
+
+    // on changed target flex
+
+    useEffect(() => {
+        const targetFlex = SafeValue(task.runtimeData?.targetFlex, -1)
+
+        if (targetFlex < 0)
+            return
+
+        flexingStartAnimation(targetFlex)
+    }, [task.runtimeData?.targetFlex])
+
+    // style
+
     const style = useMemo(() => {
         return StyleSheet.create({
             master: {
-                // flex: 1
+                backgroundColor: 'gray'
             },
 
             taskNameTxt: {
@@ -19,9 +44,12 @@ const TaskItemView = ({
     }, [])
 
     return (
-        <View>
+        <Animated.View style={[
+            style.master,
+            { flex: flexingAnimatedValue }
+        ]}>
             <Text style={style.taskNameTxt}>TaskItem</Text>
-        </View>
+        </Animated.View>
     )
 }
 
